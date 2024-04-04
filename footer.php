9936ec099a03303805c932645fd4da1425a8b072
salsasyadza <133195916+salsasyadza@users.noreply.github.com>
2024-04-04 13:02:13 +0700

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,19 @@
+<div>
+    <footer style="background-color: #006B89; width: 100%; margin-top: 80px;">
+    <div class="container p-4">
+      <div class="row">
+        <div class="col-lg-6 col-md-12 mb-4">
+          <h5 class="mb-3" style="letter-spacing: 2px; color: white">FILKOM Library</h5>
+          <p style="color: white;">
+          Web FILKOM library adalah sebuah situs web yang dirancang untuk memberikan akses kepada anggota Fakultas Ilmu Komputer (FILKOM) atau masyarakat umum untuk mengakses berbagai sumber daya informasi
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="text-center p-3" style="color: white; background-color: rgba(0, 0, 0, 0.2);">
+      © 2024 Copyright: FILKOM Library
+    </div>
+    
+  </footer>
+  
+</div>
